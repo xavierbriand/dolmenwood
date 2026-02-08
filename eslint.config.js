85a437c31e3732e0880a7dd376576398cb0dbbd7
Xavier Briand <xavierbriand@gmail.com>
2026-02-08 01:33:14 +0100

    fix: configure eslint properly and fix lint scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,19 @@
-export default [];
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    ignores: ['**/dist/**', '**/node_modules/**']
+  }
+);
